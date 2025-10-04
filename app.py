ad9c29b7e8d1d011fe4e4e18eac28e930fc44634
Bjørn <bjornsodal@gmail.com>
2025-10-04 16:40:27 +0200

    Making db

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+# Imports
+from flask import Flask, render_template, g
+import sqlite3
 
+# Configuration
+DB = 'app.db'
+
+# Initialization
 app = Flask(__name__)
 
+# DB Functions
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            DB,
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+
+        g.db.row_factory = sqlite3.Row
+    
+    return g.db
+
+def close_db():
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+app.teardown_appcontext(close_db)
+
+def init_db():
+    db = get_db()
+
+    db.executescript(""" 
+        DROP TABLE IF EXISTS 
+    """)
+# Routes
 @app.route("/")
 def home():
     return render_template("index.html")
 
+# Run app
 if __name__ == "__main__":
     app.run(debug=True)
